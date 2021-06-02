51b0d2e7535f17e6b702f163ac5cbb623442de02
TheNextUchiha <farzanpira3@gmail.com>
2021-06-02 14:39:08 +0530

    Multer modified to accept doc, pdf or any files

--- routes/fileupload.js
+++ routes/fileupload.js
@@ -1,19 +1,70 @@
 const express = require('express');
 const multer = require('multer');
 
+const router = express.Router();
+
+// ---> Multer configs <---
 const upload = multer({
     dest: 'uploads'
 });
 
-const router = express.Router();
+const uploadImg = multer({
+    dest: 'uploads',
+    limits: {
+        fileSize: 1 * 1024 * 1024
+    },
+    fileFilter(req, file, cb) {
+        if(!file.originalname.match(/\.(png|jpg|jpeg)$/)) {
+            return cb('Please upload only a Picture!');
+        }
+
+        cb(undefined, true);
+    }
+});
+
+const uploadDoc = multer({
+    dest: 'uploads',
+    limits: {
+        fileSize: 500000
+    },
+    fileFilter(req, file, cb) {
+        if(!file.originalname.match(/\.(pdf|doc|docx)$/)) {
+            cb(new Error('Please upload only a Document'));
+        }
+
+        cb(undefined, true);
+    }
+});
 
 let alert = true;
 
+// ---> GET Requests <---
 router.get('/file-upload', (req, res) => {
     res.render('fileupload');
 });
 
-router.post('/file-upload', upload.single('doc'), (req, res) => {
+router.get('/file-upload/img', (req, res) => {
+    res.render('fileuploadimg');
+});
+
+router.get('/file-upload/doc', (req, res) => {
+    res.render('fileuploadimg');
+});
+
+router.get('/file-upload/any', (req, res) => {
+    res.render('fileuploadimg');
+});
+
+// ---> POST Requests <---
+router.post('/file-upload/img', uploadImg.single('doc'), (req, res) => {
+    res.render('fileuploadimg', {alert});
+});
+
+router.post('/file-upload/doc', uploadDoc.single('doc'), (req, res) => {
+    res.render('fileupload', {alert});
+});
+
+router.post('/file-upload/any', upload.single('doc'), (req, res) => {
     res.render('fileupload', {alert});
 });
 
